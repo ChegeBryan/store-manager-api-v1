baccbfb317bb9a6bcee41d981a355a07e44a8d21
Chege Bryan <cbbryan8@gmail.com>
2018-10-18 23:00:08 +0300

    [Chore #161313394] Add unittest for app environments

--- app/tests/test_config.py
+++ app/tests/test_config.py
@@ -1,1 +1,30 @@
 # Test config.py
+
+import unittest
+
+from app import create_app
+
+
+class TestConfig(unittest.TestCase):
+    """
+    Test app configures correctly in different environments
+    """
+
+    def test_app_is_development(self):
+        app = create_app('development')
+        self.assertTrue(app.config['DEBUG'] is True)
+
+    def test_app_is_testing(self):
+        app = create_app('testing')
+        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertTrue(app.config['TESTING'] is True)
+
+    def test_app_is_production(self):
+        app = create_app('production')
+        self.assertFalse(app.config['DEBUG'] is True)
+        self.assertFalse(app.config['TESTING'] is True)
+
+
+
+
+
