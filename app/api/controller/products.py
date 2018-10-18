56a46a41d00d0e3ad0897c4bf7e2da28988614e8
Chege Bryan <cbbryan8@gmail.com>
2018-10-19 00:58:19 +0300

    [Feature #161328762] Route for getting all products

--- app/api/controller/products.py
+++ app/api/controller/products.py
@@ -1,1 +1,23 @@
 # Products endpoints
+
+from flask import request
+from flask_restplus import Resource
+
+
+from ..utils.dto import ProductDto
+from ..service.products import save_new_product, get_all_products
+
+api = ProductDto.api
+_product = ProductDto.product
+
+
+@api.route('/products')
+class ProductList(Resource):
+    """Class that defines the routes for product related HTTP methods"""
+
+    @api.response(200, 'Showing products in store')
+    @api.doc('list_of_products')
+    @api.marshal_list_with(_product, envelope='products')
+    def get(self):
+        """List all products"""
+        return get_all_products()
