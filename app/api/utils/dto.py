49d085e1879c96534e84f8254e88d03dff36608b
Chege Bryan <cbbryan8@gmail.com>
2018-10-19 00:49:47 +0300

    [Feature #161328762] Define namespace and DTO for the product

--- app/api/utils/dto.py
+++ app/api/utils/dto.py
@@ -1,1 +1,12 @@
 # carry data between processes and marshalling data for API calls
+from flask_restplus import Namespace, fields
+
+
+class ProductDto:
+    api = Namespace('product', description='product related operations')
+    product = api.model('product', {
+        'product_name': fields.String(required=True, description='product name'),
+        'mini_description': fields.String(required=True, description='product description'),
+        'description': fields.String(required=True, description='product name'),
+        'price_per_unit': fields.Integer(required=True, description='product description')
+    })
