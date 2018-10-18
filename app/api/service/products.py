20ac9c5398c2d9f9d43571c989c2ce59b222df7b
Chege Bryan <cbbryan8@gmail.com>
2018-10-19 00:46:21 +0300

    [Feature #161328762] Define functions to save producs

--- app/api/service/products.py
+++ app/api/service/products.py
@@ -1,1 +1,29 @@
 # defines the logic for the products model
+from app.api.db import mock_db
+from app.api.model.products import Product
+
+
+def save_new_product(data):
+    """Saves creation of new product if a product with the same doesnt exist"""
+
+    new_product = Product(
+        product_id=mock_db.PRODUCTS[-1]['product_id'] + 1,
+        product_name=data['product_name'],
+        mini_description=data['mini_description'],
+        description=data['description'],
+        price_per_quantity=data['price_per_quantity']
+    )
+    save_changes(new_product)
+    response_object = {
+        'status': 'success',
+        'message': 'Product added to catalog.'
+    }
+    return response_object, 201
+
+
+def get_all_products():
+    return mock_db.PRODUCTS, 200
+
+
+def save_changes(data):
+    mock_db.PRODUCTS.append(data)
