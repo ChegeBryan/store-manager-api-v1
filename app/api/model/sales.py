ed4bea2bf04bc7017aca10ae9142557e6319a0a8
Chege Bryan <cbbryan8@gmail.com>
2018-10-19 05:42:27 +0300

    [Feature #161333497] Add sale order item model

--- app/api/model/sales.py
+++ app/api/model/sales.py
@@ -5,4 +5,13 @@ class SaleOrderItems:
     """
     defines the sales model
     """
-    pass
+    def __init__(self, sale_order_items_id, product_id, quantity,
+                 price_per_unit, sale_order_id, user_id):
+        self.sale_order_items_id = sale_order_items_id
+        self.product_id = product_id
+        self.quantity = quantity
+        self.price_per_unit = price_per_unit
+        self.total_price = price_per_unit * quantity
+        self.sale_order_id = sale_order_id
+        self.user_id = user_id
+
