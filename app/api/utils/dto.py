1a6b31731031cc68acabec1f2cb63ea80bd78ae5
Chege Bryan <cbbryan8@gmail.com>
2018-10-19 06:36:35 +0300

    [Feature #161333497] Define sales namespace and DTO

--- app/api/utils/dto.py
+++ app/api/utils/dto.py
@@ -11,3 +11,15 @@ class ProductDto:
         'description': fields.String(required=True, description='detailed product description'),
         'price_per_unit': fields.Integer(required=True, description='price of one unit of the this product')
     })
+
+
+class SalesDto:
+    api = Namespace('sales', description='sales related operations')
+    sales = api.model('sales', {
+        'sale_order_items_id': fields.Integer(required=True, description='sale order item identifier'),
+        'product_id': fields.Integer(required=True, description='product identifier'),
+        'quantity': fields.Integer(required=True, description='quantity of product ordered'),
+        'price_per_unit': fields.Integer(required=True, description='1 unit price of commodity'),
+        'sale_order_id': fields.Integer(required=True, description='sale order id this order items belong to'),
+        'user_id': fields.Integer(required=True, description='user id for sale attendant')
+    })
