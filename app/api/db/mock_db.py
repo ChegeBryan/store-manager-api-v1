d2642e83ffdbcf7c8ee8726e561cd4d2506e6460
Chege Bryan <cbbryan8@gmail.com>
2018-10-21 09:18:43 +0300

    [Chore #161364902] Add method to get sale by specific id

--- app/api/db/mock_db.py
+++ app/api/db/mock_db.py
@@ -6,6 +6,7 @@ class MockDb:
     Class that hold methods that will help in mock database manipulation
     """
     PRODUCTS = []
+    SALES = []
 
     @classmethod
     def get_product_by_id(cls, _id):
@@ -18,23 +19,13 @@ class MockDb:
             if product.display_product()['product_id'] == _id:
                 return product
 
-
-SALES = [
-    {
-        "sale_id": 1,
-        "product_name": 'Mac book 2017',
-        "quantity": 2,
-        "price_per_unit": 99,
-        "total_price": 99999,
-        "sold_by": 'bryan'
-
-    },
-    {
-        "sale_id": 1,
-        "product_name": 2,
-        "quantity": 2,
-        "price_per_unit": 34,
-        "total_price": 2223,
-        "sale_by": 'bryan'
-    }
-]
+    @classmethod
+    def get_sale_by_id(cls, _id):
+        """
+        Loops through contents in the Sale list checking for an id match
+        :param _id: sale identifier
+        :return: sale with a certain id
+        """
+        for product in cls.SALES:
+            if product.display_sales()['sale_id'] == _id:
+                return product
